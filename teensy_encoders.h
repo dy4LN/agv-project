c0b5565e5a9958db8ee89c447595076f1aa44d8e
dy4LN <dylanrodriguez0117@gmail.com>
2026-03-17 19:28:08 -0700

    Create teensy_encoders.h

---
+++ teensy_encoders.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// ======================================================
+// ENCODER INTERFACE
+// ======================================================
+//
+// Handles:
+// - Encoder pin initialization
+// - Interrupt-based tick counting (implemented in .cpp)
+// ======================================================
+
+
+// Initialize encoder pins and interrupts
+void encodersInit();
